8ff1749ddec0400922ac50afc1b6a21b165f026e
Daniil Paskal <pask.daniil@gmail.com>
2025-04-27 05:19:35 -0400

    Create class for circuits

---
+++ frontend/src/classes/Circuit.js
@@ -0,0 +1,17 @@
+class Circuit {
+    qubits = [];
+    gates = [];
+
+    constructor(qubits, gates) {
+      this.qubits = qubits;
+      this.gates = gates;
+    }
+
+    addQubit(qubit) {
+        this.qubits.push(qubit);
+    }
+
+    addGate(gate) {
+      this.gates.push(gate);
+    }
+  }
