084de194b929602dbd5eeb0f04cb61feba5470ec
Daniil Paskal <pask.daniil@gmail.com>
2025-05-01 04:02:51 -0400

    Work on "simulate" endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from qiskit import QuantumCircuit, AerSimulator
+
+from circuit import Circuit
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"])
@@ -9,5 +12,18 @@ def ping():
     return {"message": "Hello World"} 
 
 @app.post("/simulate")
-def simulate():
-    return
+def simulate(data):
+    circuit = QuantumCircuit(data.qubits)
+
+    for gate in data.gates:
+        if (gate.name == "h"):
+            circuit.h(gate.target)
+        elif (gate.name == "cx"):
+            circuit.cx(gate.control, gate.target)
+
+    simulator = AerSimulator() 
+    circuit.save_statevector() 
+    result = simulator.run(circuit).result() 
+    statevector = result.get_statevector()
+    
+    return statevector
