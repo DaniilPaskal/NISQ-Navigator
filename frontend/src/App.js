2fce93ff4e4e58842244fbabc3119a91caad5561
Daniil Paskal <pask.daniil@gmail.com>
2025-05-08 23:08:48 -0400

    Work on input parsing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,9 @@ function App() {
   const [qubits, setQubits] = useState([]);
   const [inputText, setInputText] = useState("");
   const [result, setResult] = useState("");
-  const circuit = new Circuit();
-  const gates = [];
+  const [errors, setErrors] = useState([]);
 
-  const simulate = async () => {
+  const simulate = async (circuit) => {
     parseInput();
 
     await fetch('http://localhost:8080/simulate/', {
@@ -28,11 +27,28 @@ function App() {
   }
 
   const parseInput = () => {   
-    const lines = inputText.trim().split('\n');   
-    
+    const lines = inputText.trim().split('\n');
+    const gates = [];
+
     for (const line in lines) {
       const lineComponents = line.trim().split(' ');
-    }    
+
+      for (var i; i < lineComponents.length; i++) {
+        if (lineComponents[i] === "h") {
+          if (i < lineComponents.length) {
+            gates.push(new Gate("h"), parseInt(lineComponents[i + 1]));
+          }
+        } else if (lineComponents[i] === "cx") {
+          if (i < lineComponents.length - 1) {
+            gates.push(new Gate("cx"), parseInt(lineComponents[i + 1], parseInt(lineComponents[i + 2])));
+          }
+        }
+      }
+    }  
+    
+    const qubits = Math.max(...gates.flatMap(gate => [gate.target, gate.control ?? -1])) + 1;
+    const circuit = new Circuit(qubits, gates);
+    simulate(circuit);
   }
 
   return (
@@ -46,18 +62,26 @@ function App() {
       <div className="circuit">   
         {qubits.map((qubit) => (
           <div className="qubit-row" key={qubit}>
-            {gates.map((g, i) => (
-              g.target === qubit || g.control === qubit ? (           
-                <div key={i} className="gate">
-                  {g.name}
+            {gates.map((gate) => (
+              gate.target === qubit || gate.control === qubit ? (           
+                <div key={gate} className="gate">
+                  {gate.name}
                 </div>         
               ) : (
-                <div key={i} className="empty-slot" />         
+                <div key={gate} className="empty-slot" />         
               )       
             ))}     
           </div>   
         ))} 
       </div> 
+
+      {/* {errors.length > 0 && (
+        <div className="errors">
+          {errors.map((error) => 
+            <p key={error}>{error}</p>
+          )}   
+        </div>
+      )}  */}
 
       <button name="simulate" onClick={simulate}>
         Simulate
